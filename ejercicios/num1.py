b79a91e560c77cf4a99f28b7a653ee2aad06ebe8
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-15 00:31:33 -0300

    creo ejercicio 2

--- ejercicios/num1.py
+++ ejercicios/num1.py
@@ -1,3 +1,4 @@
+#imprimir los num 1 al 20
 num =1 
 for num in range(21):
     print(num)
---
+++ ejercicios/num2.py
@@ -0,0 +1,5 @@
+#imprimir la tabla del 5
+num = 5
+for num in range(51):
+    masCinco = num + 5
+    print(masCinco) 
