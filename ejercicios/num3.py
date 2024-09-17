cf02e1c00638ee3911a6a36b6c656caf6abf2d60
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-17 20:43:59 -0300

    ejercicio 3 finalizado

--- ejercicios/num3.py
+++ ejercicios/num3.py
@@ -1,6 +1,7 @@
 #imprimir la suma de numeros del 1 al 100
 
-i = 1
-for i in range(100):
-    ope = (i + i)
-    print(ope)
+suma = 0
+for i in range(1,101):
+    suma += i
+    #print("\n Esta es la suma de los numeros del 1 al 100 \n"[suma])
+    print(suma)
