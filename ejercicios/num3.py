f1d8556455646d6790714cf8c69a0595fb821e03
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-23 19:51:11 -0300

    correccion en bucle para imprimir solo el numero final

--- ejercicios/num3.py
+++ ejercicios/num3.py
@@ -3,5 +3,5 @@
 suma = 0
 for i in range(1,101):
     suma += i
-    #print("\n Esta es la suma de los numeros del 1 al 100 \n"[suma])
-    print(suma)
+    #print(f"{i} + {i+1} ={suma}")
+print(f"\n Esta es la suma de los numeros del 1 al 100 \n {suma}")
