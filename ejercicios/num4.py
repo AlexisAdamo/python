47702584c1b3e5fd2dde1019722cfe565c681726
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-23 20:16:10 -0300

    correccion en contador

--- ejercicios/num4.py
+++ ejercicios/num4.py
@@ -1,6 +1,6 @@
 #imprimir los numeros pares del 0 al 100. finalizado
 
 print(f"Los numeros pares que hay entre 0 y 100 son: ")
-for i in range(1,101):
+for i in range(101):
     if i%2 ==0:
         print(f"{i}")
