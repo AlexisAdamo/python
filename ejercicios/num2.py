05c9a0187dd78af27721e6b1742230df29019bb6
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-23 19:32:26 -0300

    agrego titulo en num2

--- ejercicios/num2.py
+++ ejercicios/num2.py
@@ -1,7 +1,7 @@
 #imprimir la tabla del 5
 multiplicador = 5
 
+print(f"Esta es la tabla del {multiplicador}")
 for ini in range(1,11):
     operacion =  ( ini * multiplicador)
-    
-    print(f"{ini} * {multiplicador} = {operacion}")
+    print(f"{multiplicador} * {ini} = {operacion}")
