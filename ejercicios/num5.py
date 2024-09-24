3d149bb550c4efa7df2b441dc0ca962c79697eb1
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-23 21:21:03 -0300

    doble solucion a num5.py

--- ejercicios/num5.py
+++ ejercicios/num5.py
@@ -1,5 +1,13 @@
 #imprimir una candena de texto con los puntos entre sus letras
-palabra = int(input("Ingrese una palabra: "))
-separador = ".".join(list(palabra))#list combierte en lista de caracteres, join los une con el simbolo "." 
+palabra = (input("Ingrese una palabra: "))
+separador = ".".join(list(palabra))#list convierte en lista de caracteres, join los une con el simbolo "." 
 print(separador)
 
+
+""""
+resuelto de manera mas simple
+palabra = (input("Ingrese una palabra: "))
+print(".".join(palabra))
+
+
+"""
