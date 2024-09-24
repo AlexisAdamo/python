b06e5ca432f1836c28d3b4565f78d3868b5415fb
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-24 20:34:38 -0300

    num7 finalizaado

--- ejercicios/num7.py
+++ ejercicios/num7.py
@@ -1,8 +1,12 @@
 #contar los digitos de un numero
-num = input(print("Ingrese un Numero"))
+
+'''num = input(print("Ingrese un Numero"))
 
 if num != int:
-    print(f"ATENCION! {num} no es un dato valido, /n Por favor ingrese un valor numerico")
+    return (f"ATENCION! {num} no es un dato valido, /n Por favor ingrese un valor numerico")
 else:
     print(f"El numero {num} contiene {len(str(num))} digitos")
-    
+'''
+
+num = 123
+print(f"El numero {num} contiene {len(str(num))} digitos")
