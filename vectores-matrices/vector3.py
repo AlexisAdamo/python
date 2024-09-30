6ddca629f6f4a1c6a49f4d2e58c28ca988e6a485
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-30 19:50:22 -0300

    vec3  terminado

---
+++ vectores-matrices/vector3.py
@@ -0,0 +1,7 @@
+#Multiplica todos los elementos del vector por 2.
+
+import numpy as np
+vector = [5,3,2,6]
+print("vector original:", vector)
+ope = np.multiply(vector,2)
+print("vector multiplicado por 2: ",ope)
