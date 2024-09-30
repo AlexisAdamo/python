01e0ff1a60a89e884a05288fd3634022b3f70735
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-30 20:23:15 -0300

    fix in logic vec1-2

--- vectores-matrices/vector1-2-.py
+++ vectores-matrices/vector1-2-.py
@@ -1,13 +1,13 @@
 #Cambia el valor del quinto elemento del vector por 0.
-
-import random
+import numpy as np
+#import random
 #random > biblioteca que permite generar numeros aleatorios
 print("Este es un vector de 10 elementos con numeros del 1 al 100")
-vector = [random.randint(1,100) for i in range(10)]
+vector = [np.random.randint(1,100) for i in range(9)]
 '''
 random.randit(1,100) > genera un numero aleatorio para cada elemento de vector 
 '''
-vector2 = (vector.insert(5,0))
+vector2 = (vector.insert(4,0))
 print(vector)
 #print(vector2)
 
