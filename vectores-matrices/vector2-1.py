4bd2bdfd50b1b990674b096689a57c19e3b1561c
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-30 21:05:31 -0300

    vec 1-2 y2-1

---
+++ vectores-matrices/vector2-1.py
@@ -0,0 +1,10 @@
+#Crea dos matrices de 3x3 con valores aleatorios entre 1 y 10.
+import numpy as np
+
+matriz1 = np.random.randint(1,11,(3,3))
+matriz2 = np.random.randint(1,11,(3,3))
+print("matriz 1: ")
+print(matriz1)
+print("matriz 2: ")
+print(matriz2)
+
