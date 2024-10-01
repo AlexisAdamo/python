1fef6adc08d4641b91dea4103154e3c49941f707
AlexisAdamo <aleadamo.1908@gmail.com>
2024-09-30 21:10:35 -0300

    comment added in function ramdom rendint

--- vectores-matrices/vector2-1.py
+++ vectores-matrices/vector2-1.py
@@ -1,7 +1,7 @@
 #Crea dos matrices de 3x3 con valores aleatorios entre 1 y 10.
 import numpy as np
 
-matriz1 = np.random.randint(1,11,(3,3))
+matriz1 = np.random.randint(1,11,(3,3))# random.randint = numeros enteros random.
 matriz2 = np.random.randint(1,11,(3,3))
 print("matriz 1: ")
 print(matriz1)
