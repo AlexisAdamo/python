c74a47c14ad5792db63ac7e4d1826a4266d5124b
AlexisAdamo <aleadamo.1908@gmail.com>
2024-10-04 18:48:26 -0300

    lista4-1.py finish

---
+++ listas-tuplas-conjuntos-diccionarios/listas4-1.py
@@ -0,0 +1,10 @@
+#4. Contar la cantidad de elementos en una lista:
+#Crea una lista con al menos 10 elementos.
+#Cuenta cuántos elementos tiene la lista y muestra el resultado.
+
+
+lista = [1,5,3,8,9,"boca", "pelota", "pasto", 45,67,12]
+numElementos = len(lista)
+print(f"lista original: \n", lista)
+
+print(f"la lista contine {numElementos} elementos")
